b2a6f7d8f51842cb2bdcb651ea5551c08c4aec96
TNT_WSL <>
2025-03-24 19:23:52 +0530

    Improved Factory

--- src/pieces/Piece.cpp
+++ src/pieces/Piece.cpp
@@ -1,9 +1,9 @@
 #include "Rook.cpp"
 #include "Knight.cpp"
 
-BasePiece::BasePiece(COLOUR c, const std::string& texturePath, std::shared_ptr<MovementStrategy> strategy)
+BasePiece::BasePiece(COLOUR c, const std::string& texturecolor_path, std::shared_ptr<MovementStrategy> strategy)
     : color(c), movementStrategy(strategy) 
-    {texture = LoadTexture(texturePath.c_str());}
+    {texture = LoadTexture(texturecolor_path.c_str());}
 
 void BasePiece::Draw(Vector2 position) const {
         float scale = 0.35f;
@@ -17,11 +17,15 @@ bool BasePiece::validateMove(Board& board, Vector2 from, Vector2 to) {
 BasePiece::~BasePiece() { UnloadTexture(texture); }
 
 std::unique_ptr<Piece> Piece::PieceFactory(PieceType type, COLOUR color, std::shared_ptr<MovementStrategy> sharedStrategy) {
+    
+    std::string color_path = (color == COLOUR::W ? "w" : "b");
+    std::string path ="assets/textures/" + color_path;
+
     switch(type) {
         case PieceType::ROOK: 
-            return color == COLOUR::W ? std::make_unique<Rook>(color, "assets/textures/wr.png", sharedStrategy) : std::make_unique<Rook>(color, "assets/textures/br.png",sharedStrategy); 
+            return std::make_unique<Rook>(color, path+"r.png", sharedStrategy);
         case PieceType::KNIGHT: 
-            return color == COLOUR::W ? std::make_unique<Knight>(color, "assets/textures/wn.png", sharedStrategy) : std::make_unique<Knight>(color, "assets/textures/bn.png",sharedStrategy);
+            return std::make_unique<Knight>(color, path+"n.png", sharedStrategy);
         default: return nullptr;
     }
 }
